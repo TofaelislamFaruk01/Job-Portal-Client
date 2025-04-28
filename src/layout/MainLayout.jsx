86b589db7e9fe2a160c715eecfdae1c3b9e68d22
Tofael Islam Faruk <tofaelfaruk@gmail.com>
2025-04-28 09:00:16 +0600

    Auth route setup and folder structure

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../pages/Shared/Navbar';
+
+const MainLayout = () => {
+    return (
+        <div className='max-w-7xl mx-auto'>
+            <Navbar/>
+            <Outlet/>
+            
+        </div>
+    );
+};
+
+export default MainLayout;
