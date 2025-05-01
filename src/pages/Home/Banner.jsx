24018be30ac407e8496105cc225e8c89204b5232
Tofael Islam Faruk <tofaelfaruk@gmail.com>
2025-05-01 15:30:52 +0600

    Add banner section to homepage

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,18 +1,47 @@
 import { motion } from "motion/react"
 import React from 'react';
+import img1 from '../../assets/people-working-office.jpg'
+import img2 from '../../assets/business-plan-office.jpg'
 
 const Banner = () => {
     return (
         <div className="hero bg-base-200 min-h-96">
   <div className="hero-content flex-col lg:flex-row-reverse">
+  <div className='flex-1'>
+  <motion.img
+    src={img1}
+    className="max-w-sm rounded-t-[40px] rounded-br-[40px] border-l-8 border-b-8 border-blue-300  shadow-2xl"
+    animate={{ y: [50, 100, 50] }} // Moves down then up
+    transition={{
+      duration: 4,
+      repeat: Infinity,
+      repeatType: "loop",
+      ease: "easeInOut",
+    }}
+  />
+  <motion.img
+    src={img2}
+    className="max-w-sm rounded-t-[40px] rounded-br-[40px] border-l-8 border-b-8 border-blue-300 shadow-2xl"
+    animate={{ x: [60, 10, 60] }} // Moves left then right
+    transition={{
+      duration: 4,
+      repeat: Infinity,
+      repeatType: "loop",
+      ease: "easeInOut",
+    }}
+  />
+</div>
+
     <div className='flex-1'>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl"
-    />
-    </div>
-    <div className='flex-1'>
-      <h1 className="text-5xl font-bold">Latest <motion.div>Jobs</motion.div> For You!</h1>
+      <h1 className="text-5xl font-bold">Latest <motion.span 
+       animate={{ color: ["#FF5733", "#33FF57", "#3357FF"] }}
+       transition={{
+         duration: 4.5, // total duration for 3 colors
+         repeat: Infinity,
+         repeatType: "loop",
+         ease: "linear",
+       }}
+        >Jobs</motion.span> For You!</h1>
       <p className="py-6">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.
