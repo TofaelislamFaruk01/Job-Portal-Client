62ed9a882bb70debcb420027b44fa5fbe775e608
Tofael Islam Faruk <tofaelfaruk@gmail.com>
2025-05-06 09:21:51 +0600

    Use Custom hook and created post api for Job application

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,45 +1,82 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+import Swal from 'sweetalert2';
 
 const JobApply = () => {
 
-    const handleSubmit=(e)=>{
+    const {id} = useParams();
+
+    const {user} = useAuth();
+
+    console.log(id,user.email);
+
+    const handleSubmit = (e) => {
         e.preventDefault();
         const form = e.target;
         const github = form.github.value;
-        const linkedIn=form.linkedIn.value;
+        const linkedIn = form.linkedIn.value;
         const resume = form.resume.value;
-        
-        console.log(github,linkedIn,resume);
+
+        console.log(github, linkedIn, resume);
+
+        const jobApplication = {
+            job_id : id,
+            applicant_email:user.email,
+            linkedIn,
+            github,
+            resume
+        }
+
+        fetch('http://localhost:3000/job-applications',{
+            method:'POST',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body:JSON.stringify(jobApplication)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            if(data.insertedId)
+            {
+                Swal.fire({
+                    title: "Your application has been submitted!",
+                    icon: "success",
+                    draggable: true
+                  });
+
+                  form.reset();
+
+            }
+            
+
+        }
+        )
     }
     return (
-        <div>
-           <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-    </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form className="card-body" onSubmit={handleSubmit}>
-        <fieldset className="fieldset">
-          <label className="label">LinkedIn URL</label>
-          <input type="url" className="input" placeholder="LinkedIn url" name='linkedIn' />
-          <label className="label">Github URL</label>
-          <input type="url" className="input" placeholder="Github URL" name='github'/>
-          <label className="label">Resume URL</label>
-          <input type="url" className="input" placeholder="Resume URL" name='resume'/>
-          
-          <button className="btn btn-neutral mt-4">Submit</button>
-        </fieldset>
-      </form>
-    </div>
-  </div>
-</div>
-            
-        </div>
+       
+
+           
+            <div className="card bg-base-100 w-full max-w-2xl shadow-2xl my-10 mx-auto">
+                <form className="card-body" onSubmit={handleSubmit}>
+                    <h1 className="text-5xl font-bold mb-8">Apply Job and Good Luck!</h1>
+                    <fieldset className="fieldset">
+                        <label className="label">LinkedIn URL</label>
+                        <input type="url" className="input w-full" placeholder="LinkedIn url" name='linkedIn' />
+                        <label className="label">Github URL</label>
+                        <input type="url" className="input w-full" placeholder="Github URL" name='github' />
+                        <label className="label">Resume URL</label>
+                        <input type="url" className="input w-full" placeholder="Resume URL" name='resume' />
+
+                        <button className="btn btn-neutral mt-4">Submit</button>
+                    </fieldset>
+                </form>
+            </div>
+       
+
+
+
     );
 };
 
